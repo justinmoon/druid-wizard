5d435db909758fd8220db9771e69ab053423841c
Justin Moon <mail@justinmoon.com>
2020-08-26 22:58:48 -0500

    Druid-enums wizard

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,122 @@
-use druid::widget::{Align, Flex, Label, TextBox};
-use druid::{AppLauncher, Data, Env, Lens, LocalizedString, Widget, WidgetExt, WindowDesc};
+use druid::{
+    widget::{Button, Controller, Flex, Label, TextBox},
+    AppLauncher, Data, Env, Event, EventCtx, Lens, PlatformError, Selector, Widget, WidgetExt,
+    WindowDesc,
+};
+use druid_enums::Matcher;
 
-const VERTICAL_WIDGET_SPACING: f64 = 20.0;
-const TEXT_BOX_WIDTH: f64 = 200.0;
-const WINDOW_TITLE: LocalizedString<HelloState> = LocalizedString::new("Hello World!");
+const NEXT: Selector = Selector::new("wizard.next");
+const BACK: Selector = Selector::new("wizard.back");
 
-#[derive(Clone, Data, Lens)]
-struct HelloState {
+#[derive(Clone, Data, Matcher)]
+#[matcher(matcher_name = App)] // defaults to WizardMatcher
+enum Wizard {
+    Name(NameState),
+    Age(AgeState),
+}
+
+#[derive(Clone, Data, Lens, Default)]
+struct NameState {
     name: String,
 }
 
-pub fn main() {
-    // describe the main window
-    let main_window = WindowDesc::new(build_root_widget)
-        .title(WINDOW_TITLE)
-        .window_size((400.0, 400.0));
-
-    // create the initial app state
-    let initial_state = HelloState {
-        name: "World".into(),
-    };
-
-    // start the application
-    AppLauncher::with_window(main_window)
-        .launch(initial_state)
-        .expect("Failed to launch application");
+impl NameState {
+    fn done(&self) -> bool {
+        self.name.len() > 0
+    }
 }
 
-fn build_root_widget() -> impl Widget<HelloState> {
-    // a label that will determine its text based on the current app data.
-    let label = Label::new(|data: &HelloState, _env: &Env| format!("Hello {}!", data.name));
-    // a textbox that modifies `name`.
-    let textbox = TextBox::new()
-        .with_placeholder("Who are we greeting?")
-        .fix_width(TEXT_BOX_WIDTH)
-        .lens(HelloState::name);
-
-    // arrange the two widgets vertically, with some padding
-    let layout = Flex::column()
-        .with_child(label)
-        .with_spacer(VERTICAL_WIDGET_SPACING)
-        .with_child(textbox);
-
-    // center the two widgets in the available space
-    Align::centered(layout)
+#[derive(Clone, Data, Lens)]
+struct AgeState {
+    name: String,
+    age: String,
 }
+
+impl AgeState {
+    fn done(&self) -> bool {
+        self.age.len() > 0
+    }
+}
+
+fn main() -> Result<(), PlatformError> {
+    let window = WindowDesc::new(ui).title("Druid Enums");
+    let state = Wizard::Name(NameState::default());
+    AppLauncher::with_window(window)
+        .use_simple_logger()
+        .launch(state)
+}
+
+fn ui() -> impl Widget<Wizard> {
+    // Wizard::matcher() or
+    App::new()
+        .age(age_ui())
+        .name(name_ui())
+        .controller(WizardController)
+}
+
+fn name_ui() -> impl Widget<NameState> {
+    fn next(ctx: &mut EventCtx, state: &mut NameState, _: &Env) {
+        if state.done() {
+            ctx.submit_command(NEXT, None)
+        }
+    }
+
+    Flex::row()
+        .with_child(Label::new("Name"))
+        .with_child(TextBox::new().lens(NameState::name))
+        .with_spacer(5.0)
+        .with_child(Button::new("Next").on_click(next))
+        .center()
+}
+
+fn age_ui() -> impl Widget<AgeState> {
+    fn next(ctx: &mut EventCtx, state: &mut AgeState, _: &Env) {
+        if state.done() {
+            ctx.submit_command(NEXT, None)
+        }
+    }
+    Flex::row()
+        .with_child(Label::new("Age"))
+        .with_child(TextBox::new().lens(AgeState::age))
+        .with_spacer(5.0)
+        .with_child(Button::new("Next").on_click(next))
+        .center()
+}
+
+struct WizardController;
+impl Controller<Wizard, App> for WizardController {
+    fn event(
+        &mut self,
+        child: &mut App,
+        ctx: &mut EventCtx,
+        event: &Event,
+        data: &mut Wizard,
+        env: &Env,
+    ) {
+        match event {
+            Event::Command(cmd) if cmd.is(NEXT) => data.next(),
+            _ => {}
+        }
+        child.event(ctx, event, data, env)
+    }
+}
+
+impl Wizard {
+    fn next(&mut self) {
+        match self {
+            Wizard::Name(name_state) => *self = Wizard::Age(AgeState::from(name_state.clone())),
+            Wizard::Age(age_state) => println!("done"),
+        }
+    }
+}
+
+impl From<NameState> for AgeState {
+    fn from(name_state: NameState) -> Self {
+        AgeState {
+            name: name_state.name,
+            age: "".to_string(),
+        }
+    }
+}
+
 
